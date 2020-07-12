94ac6fa1f39d896ba645df33673080c9acd81836
Radhika Patel <radz@Radhikas-MacBook-Air.local>
2020-07-12 10:29:19 +0530

    Added check in user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
 
 const CONFIG = require("../../config.json");
 const TOKEN_SECRET = CONFIG.token.secret;
@@ -9,100 +10,144 @@ const TOKEN_EXPIRES = parseInt(CONFIG.token.expiresInSeconds, 10);
 
 const User = require("../models/user");
 
-router.post("/", (req, res) => {
-    // Check if User with that username already exists or not
-    User.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-            res.status(500).json({
-                message: "Internal Server Error.",
-            });
+router.post(
+    "/",
+    [
+        check(
+            ["firstName", "lastName", "username", "password"],
+            "firstName | lastName | username | password must be provided (String)"
+        )
+            .not()
+            .isEmpty()
+            .isString(),
+    ],
+    (req, res) => {
+        // Validate request data
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).jsonp(errors.array());
+        } else {
+            // Check if User with that username already exists or not
+            User.findOne({ username: req.body.username }, (err, user) => {
+                if (err) {
+                    res.status(500).json({
+                        message: "Internal Server Error.",
+                    });
 
-            return;
-        }
+                    return;
+                }
 
-        // If User with that username exists, throw error (Conflict)
-        if (user) {
-            res.status(409).json({
-                message: `User with the username ${req.body.username} already exists.`,
-            });
+                // If User with that username exists, throw error (Conflict)
+                if (user) {
+                    res.status(409).json({
+                        message: `User with the username ${req.body.username} already exists.`,
+                    });
 
-            return;
-        }
+                    return;
+                }
 
-        // If User with that username does not exists, create new entry in DB for that user
-        const salt = bcrypt.genSaltSync(10);
-        const hashPassword = bcrypt.hashSync(req.body.password, salt);
+                // If User with that username does not exists, create new entry in DB for that user
+                const salt = bcrypt.genSaltSync(10);
+                const hashPassword = bcrypt.hashSync(req.body.password, salt);
 
-        var user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            username: req.body.username,
-            password: hashPassword,
-        });
-
-        User.create(user, (err, user) => {
-            if (err) {
-                res.status(500).json({
-                    message: "Internal Server Error.",
+                var user = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    username: req.body.username,
+                    password: hashPassword,
                 });
 
-                throw err;
-            } else {
-                // Return User data after successful DB entry creation
+                User.create(user, (err, user) => {
+                    if (err) {
+                        res.status(500).json({
+                            message: "Internal Server Error.",
+                        });
+
+                        throw err;
+                    } else {
+                        // Return User data after successful DB entry creation
+                        res.status(200).json({
+                            message: `User ${user.username} created successfully.`,
+                            data: {
+                                firstName: user.firstName,
+                                lastName: user.lastName,
+                                username: user.username,
+                                createdAt: user.createdAt,
+                            },
+                        });
+                    }
+                });
+            });
+        }
+    }
+);
+
+router.post(
+    "/login",
+    [
+        check(
+            ["username", "password"],
+            "username | password must be provided (String)."
+        )
+            .not()
+            .isEmpty()
+            .isString(),
+    ],
+    (req, res) => {
+        // Validate request data
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).jsonp(errors.array());
+        } else {
+            // Check if User with that username exists or not
+            User.findOne({ username: req.body.username }, (err, user) => {
+                if (err) {
+                    res.status(500).json({
+                        message: "Internal Server Error.",
+                    });
+
+                    return;
+                }
+
+                // If no User is found with that username, throw error (Unauthorized)
+                if (!user) {
+                    res.status(401).json({
+                        message: `User with username ${req.body.username} not found.`,
+                    });
+
+                    return;
+                }
+
+                // If User found, check password and throw error if incorrect (Unauthorized)
+                const result = bcrypt.compareSync(
+                    req.body.password,
+                    user.password
+                );
+                if (!result) {
+                    res.status(401).json({
+                        message:
+                            "User Authentication failed due to wrong password.",
+                    });
+
+                    return;
+                }
+
+                // User is authenticated, create token and send to the user
+                const token = jwt.sign(
+                    { username: user.username },
+                    TOKEN_SECRET,
+                    {
+                        expiresIn: TOKEN_EXPIRES,
+                    }
+                );
+
                 res.status(200).json({
-                    message: `User ${user.username} created successfully.`,
-                    data: {
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                        username: user.username,
-                        createdAt: user.createdAt,
-                    },
+                    message: "Login successful.",
+                    token: token,
                 });
-            }
-        });
-    });
-});
-
-router.post("/login", (req, res) => {
-    // Check if User with that username exists or not
-    User.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-            res.status(500).json({
-                message: "Internal Server Error.",
             });
-
-            return;
         }
-
-        // If no User is found with that username, throw error (Unauthorized)
-        if (!user) {
-            res.status(401).json({
-                message: `User with username ${req.body.username} not found.`,
-            });
-
-            return;
-        }
-
-        // If User found, check password and throw error if incorrect (Unauthorized)
-        const result = bcrypt.compareSync(req.body.password, user.password);
-        if (!result) {
-            res.status(401).json({
-                message: "User Authentication failed due to wrong password.",
-            });
-
-            return;
-        }
-
-        // User is authenticated, create token and send to the user
-        const token = jwt.sign({ username: user.username }, TOKEN_SECRET, {
-            expiresIn: TOKEN_EXPIRES,
-        });
-
-        res.status(200).json({
-            message: "Login successful.",
-            token: token,
-        });
-    });
-});
+    }
+);
 
 module.exports = router;
